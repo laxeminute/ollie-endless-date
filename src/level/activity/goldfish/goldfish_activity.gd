57b8353dd3f389777d63ed2d3f04f3857da2f7b4
VernalUmbrella <vernalumbrella@gmail.com>
2025-05-23 06:07:03 -0400

    Goldfish activity: catcher follows mouse

--- src/level/activity/goldfish/goldfish_activity.gd
+++ src/level/activity/goldfish/goldfish_activity.gd
@@ -1,1 +1,16 @@
 extends Node2D
+
+const HEIGHT = 192
+const WIDTH = 192
+const HALF_HEIGHT = HEIGHT / 2.0
+const HALF_WIDTH = WIDTH / 2.0
+
+@onready var catcher: Area2D = $Catcher
+@onready var catcher_radius: float = $Catcher/CollisionShape2D.shape.radius
+
+
+func _process(_delta: float) -> void:
+	catcher.position = get_local_mouse_position().clamp(
+		Vector2(-HALF_WIDTH + catcher_radius, -HALF_HEIGHT + catcher_radius),
+		Vector2(HALF_WIDTH - catcher_radius, HALF_HEIGHT - catcher_radius)
+	)
