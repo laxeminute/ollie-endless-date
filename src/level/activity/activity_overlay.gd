e9288589a32b4b83353f423605048477dbe9d5e5
VernalUmbrella <vernalumbrella@gmail.com>
2025-05-24 06:51:55 -0400

    Rework some activity stuff
    WARN: I'm setting the mouse filter of ActivityOverlay to "Pass" so that
    activities can get unhandled input, this *may* cause issues in the
    future.

--- src/level/activity/activity_overlay.gd
+++ src/level/activity/activity_overlay.gd
@@ -4,19 +4,49 @@ extends Control
 signal completed
 signal cancelled
 
-@export var activity_scene: PackedScene
+const ACTIVITY_HEIGHT = 192
 
-@onready var activity_container: Container = $ActivityContainer
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export var activity_scene: PackedScene
+@export var intro_tween_duration: float = 0.5
+@export var exit_tween_duration: float = 0.3
+
+var current_activity: Node2D
+var offscreen_point: Vector2:
+	get:
+		return Vector2(size.x / 2, size.y + ACTIVITY_HEIGHT)
 
 
 func _ready() -> void:
-	pass
+	open()
+
+
+func open(which: PackedScene = activity_scene):
+	assert(
+		not is_instance_valid(current_activity), "Tried to open activity when one is already active"
+	)
+	show()
+	current_activity = which.instantiate()
+	current_activity.completed.connect(completed.emit)
+	add_child(current_activity)
+	current_activity.process_mode = Node.PROCESS_MODE_DISABLED
+	var intro_tween := create_tween()
+	intro_tween.set_ease(Tween.EASE_OUT)
+	intro_tween.set_trans(Tween.TRANS_BACK)
+	intro_tween.tween_property(current_activity, "position", size / 2, intro_tween_duration).from(
+		offscreen_point
+	)
+	intro_tween.tween_callback(current_activity.set.bind("process_mode", Node.PROCESS_MODE_INHERIT))
 
 
 func close() -> void:
-	pass
+	current_activity.process_mode = Node.PROCESS_MODE_DISABLED
+	var exit_tween := create_tween()
+	exit_tween.set_ease(Tween.EASE_IN)
+	exit_tween.set_trans(Tween.TRANS_BACK)
+	exit_tween.tween_property(current_activity, "position", offscreen_point, exit_tween_duration)
+	exit_tween.tween_callback(cancelled.emit)
+	exit_tween.tween_callback(hide)
 
 
 func _on_cancel_button_pressed() -> void:
-	cancelled.emit()
+	close()
--- src/level/activity/goldfish/goldfish_activity.gd
+++ src/level/activity/goldfish/goldfish_activity.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal completed
+
 const HEIGHT = 192
 const WIDTH = 192
 const HALF_HEIGHT = HEIGHT / 2.0
@@ -9,8 +11,20 @@ const HALF_WIDTH = WIDTH / 2.0
 @onready var catcher_radius: float = $Catcher/CollisionShape2D.shape.radius
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("activity_action"):
+		_catch()
+
+
 func _process(_delta: float) -> void:
 	catcher.position = get_local_mouse_position().clamp(
 		Vector2(-HALF_WIDTH + catcher_radius, -HALF_HEIGHT + catcher_radius),
 		Vector2(HALF_WIDTH - catcher_radius, HALF_HEIGHT - catcher_radius)
 	)
+
+
+func _catch() -> void:
+	# if catcher is catching return
+	await catcher.catch()
+	if catcher.has_overlapping_areas():
+		completed.emit()
